7fd820606b06ff22e5c757b2e22f06f1b3bbf8f7
Marc Stiller <marc.stiller@gmail.com>
2018-06-26 18:35:56 -0400

    Testing MAX31865 integration

--- AutoSmoker.py
+++ AutoSmoker.py
@@ -40,7 +40,5 @@ else:
 mylcd.lcd_display_string("Temp: %dC" % temperature,1)
 mylcd.lcd_display_string("Humidity: %d%%" % humidity,2)
 
-#max = MAX31865.max31865(8,9,10,11)
-checking = help(MAX31865)
-
-print(checking)
+max = MAX31865.max31865()
+print(max)
