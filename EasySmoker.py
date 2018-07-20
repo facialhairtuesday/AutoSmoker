e7158f6fed4a97327dba870049fbd16f79449085
Marc Stiller <marc.stiller@gmail.com>
2018-07-20 18:37:36 -0400

    Modified PID code

--- EasySmoker.py
+++ EasySmoker.py
@@ -80,4 +80,7 @@ while True:
     mylcd.lcd_display_string("Pointy: %0.1fC" % pointyTemp,1)
     mylcd.lcd_display_string("Blunt: %0.1fC" % bluntTemp,2)
 
-    time.sleep(5)
+
+
+
+    time.sleep(TempInterval)
